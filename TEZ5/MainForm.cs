0957b714abd65682e54aacd26f01eb1396576f20
proxycommiepony <proxycommiepony@gmail.com>
2020-08-07 14:30:53 +0700

    Обработка ситуации без БД

--- TEZ5/MainForm.cs
+++ TEZ5/MainForm.cs
@@ -11,21 +11,30 @@ namespace TEZ5
         public MainForm()
         {
             InitializeComponent();
-            string path = Path.GetFullPath("Database.mdf");
-            DataController.setConn(path);
-            MapController.setConn(path);
-            DataController.setData(this.dataGridView);
-            DataController.loadSubs(this.substanceComboBox);
-            dataGridView.CellValueChanged += CellValueChanged;
-            pictureBox.Image = new Bitmap(Properties.Resources.RozaVetrov);
-            MapController.LoadMap(gMapControl);
-            MapController.Update(this);
-            pictureBox1.BackColor = Color.FromArgb(140, 255, 0, 0);
-            pictureBox2.BackColor = Color.FromArgb(140, 255, 128, 0);
-            pictureBox3.BackColor = Color.FromArgb(140, 255, 255, 0);
-            pictureBox4.BackColor = Color.FromArgb(140, 21, 144, 100);
-            pictureBox5.BackColor = Color.FromArgb(140, 0, 255, 0);
+            string path = Path.GetFullPath("Databas1e.mdf");
+            if (File.Exists(path))
+            {
+                DataController.setConn(path);
+                MapController.setConn(path);
+                DataController.setData(this.dataGridView);
+                DataController.loadSubs(this.substanceComboBox);
+                dataGridView.CellValueChanged += CellValueChanged;
+                pictureBox.Image = new Bitmap(Properties.Resources.RozaVetrov);
+                MapController.LoadMap(gMapControl);
+                MapController.Update(this);
+                pictureBox1.BackColor = Color.FromArgb(140, 255, 0, 0);
+                pictureBox2.BackColor = Color.FromArgb(140, 255, 128, 0);
+                pictureBox3.BackColor = Color.FromArgb(140, 255, 255, 0);
+                pictureBox4.BackColor = Color.FromArgb(140, 21, 144, 100);
+                pictureBox5.BackColor = Color.FromArgb(140, 0, 255, 0);
+            }
+            else
+            {              
+                
+                MessageBox.Show("База данных не найдена");
+            }
         }
+       
 
         private void buttonDel_Click(object sender, EventArgs e)
         {
